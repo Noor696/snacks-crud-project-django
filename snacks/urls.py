adffee077be13c16a86c81d7edb29078c426bf19
Noor696 <eng.nooralhateeb@gmail.com>
2022-12-06 10:28:58 +0000

    Update item / post method

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path  # 1. import any thing built in django
-from .views import HomePage,SnackListView, SnackDetailView , SnackCreateView # 2. import from yout application
+from .views import HomePage,SnackListView, SnackDetailView , SnackCreateView, SnackUpdateView # 2. import from yout application
 
 urlpatterns = [
     path('', HomePage.as_view(), name="home"),
     path('snacks', SnackListView.as_view(),name="snacks"),
-    path('snacks/<pk>', SnackDetailView.as_view(),name="snack_detail"),
-    path('create/', SnackCreateView.as_view(),name="snack_create")
+    path('snacks/<int:pk>', SnackDetailView.as_view(),name="snack_detail"),
+    path('create/', SnackCreateView.as_view(),name="snack_create"),
+    path('update/<int:pk>', SnackUpdateView.as_view(),name="snack_update")
 ]
 
 # snacks/<pk> : this dunamic path refer uniqe type of snackes 
